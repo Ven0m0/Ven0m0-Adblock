8bdf28ffb3d6cdf6ed4cd51432b887039691c01e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-23 10:45:01 +0200

    Update hostlist-build.sh

--- Scripts/hostlist-build.sh
+++ Scripts/hostlist-build.sh
@@ -12,3 +12,5 @@ hostlist-compiler -c hostlist-config.json -o Filters/filter.txt --verbose
 # Compiling AdGuard DNS Popup Hosts filter
 hostlist-compiler -c configuration_popup_filter.json -o Filters/adguard_popup_filter.txt --verbose
 node scripts/popup_filter_build.js Filters/adguard_popup_filter.txt
+
+hostlist-compiler -i 
