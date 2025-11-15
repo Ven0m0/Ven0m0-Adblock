12e15bfe228658c4ac27d4b6624502f0b85066b0
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 21:05:53 +0100

    Update esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  entryPoints: [],
   bundle: true,
   minify: true,
-  target: ["es2022"],
+  target: "es2022",
   platform: "browser",
-  legalComments: "none", // Remove all comments (metadata is manually preserved)
+  format: "iife",
+  legalComments: "none",
   treeShaking: true,
   charset: "utf8",
-  format: "iife", // Userscripts: avoid polluting global scope
-  logLevel: "error",
+  logLevel: "error"
 };
